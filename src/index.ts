2e3d92c17f2d198d77efae32fde4a081d7e2e76b
Lanza <77981833+Lanzadrd@users.noreply.github.com>
2023-05-16 12:33:12 +0200

    Fix cards showing up on html

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,59 @@
 import { createCharacters } from "./characters.ts";
+import { King } from "./king.ts";
+import { Warrior } from "./warrior.ts";
+import { Adviser } from "./adviser.ts";
+import { Squire } from "./squire.ts";
+import "../styles.css";
 
 const charactersArray = createCharacters();
-const template = document.querySelector(".characters-list")!;
+const html = document.querySelector(".characters-list")!;
 
-const joffreyCard = `
-        <li class="character col">
-         <div class="card character__card">
-           <img src="img/no-one.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
-           <div class="card-body">
-             <h2 class="character__name card-title h4">${charactersArray[0].name} y ${charactersArray[0].family}</h2>
-             <div class="character__info">
-               <ul class="list-unstyled">
-                 <li>Edad: ${charactersArray[0].age} años</li>
-                 <li>
-                   Estado:
-                   <i class="fas fa-thumbs-down"></i>
-                   <i class="fas fa-thumbs-up"></i>
-                 </li>
-               </ul>
-             </div>
-             <div class="character__overlay">
-               <ul class="list-unstyled">
-                 <li>Años de reinado: X</li>
-                 <li>Arma: XXX</li>
-                 <li>Destreza: X</li>
-                 <li>Peloteo: X</li>
-                 <li>Asesora a: X</li>
-                 <li>Sirve a: X</li>
-               </ul>
-               <div class="character__actions">
-                 <button class="character__action btn">habla</button>
-                 <button class="character__action btn">muere</button>
-               </div>
-             </div>
-           </div>
-           <i class="emoji"></i>
-         </div>
-       </li>
-       `;
+const template = (character: King | Warrior | Adviser | Squire) => {
+  return `
+  <li class="character col">
+  <div class="card character__card">
+  <img src="img/${character.name.toLowerCase()}.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+  <div class="card-body">
+  <h2 class="character__name card-title h4">${character.name} y ${
+    character.family
+  }</h2>
+  <div class="character__info">
+  <ul class="list-unstyled">
+  <li>Edad: ${character.age} años</li>
+  <li>
+  Estado:
+  <i class="fas fa-thumbs-down"></i>
+  <i class="fas fa-thumbs-up"></i>
+  </li>
+  </ul>
+  </div>
+  <div class="character__overlay">
+  <ul class="list-unstyled">
+  <li>Años de reinado: }</li>
+  </ul>
+  <div class="character__actions">
+  <button class="character__action btn">habla</button>
+  <button class="character__action btn">muere</button>
+  </div>
+  </div>
+  </div>
+  <i class="emoji"></i>
+  </div>
+  </li>
+  `;
+};
 
-template.innerHTML = joffreyCard;
+let htmlList = "";
+
+charactersArray.forEach((item) => {
+  console.log(item);
+  htmlList += template(item);
+});
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="app-container">
+    <ul class="characters-list row list-unstyled">
+    ${htmlList}
+    </ul>
+  </div>
+`;
